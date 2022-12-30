a12ce07cc08931843f8cd2e79e3ed4671c99985e
Kristjan <kristjan.maysoln@gmail.com>
2022-12-30 12:36:14 +0100

    Add network and parameter structure

--- main.py
+++ main.py
@@ -4,13 +4,18 @@ A program to demonstrate a three layer perceptron and backpropagation.
 Author:
 Kristjan Šoln
 """
+import network
 import numpy as np
+
 
 if __name__ == '__main__':
     # The XOR problem
 
     # Test data
-    X = np.array([[0, 0], [0, ], [1, 0], [1, 1]])  # Input data
+    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])  # Input data
     y_hat = np.array([0, 1, 1, 0])  # Validation data
 
-    
+    perc = network.Perceptron([5, 4, 3, 2])
+
+
+
--- network.py
+++ network.py
@@ -7,8 +7,31 @@ Author:
 Kristjan Šoln
 """
 
+import numpy as np
+
 
 class Perceptron(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, layer_sizes):
+        self.number_of_layers = 4  # input layer + 2 neuron layers + output layer
+        self.sizes = layer_sizes
+        # Biases:
+        self.biases = []
+        for layer_size in layer_sizes[1:]:
+            self.biases.append(np.random.randn(layer_size, 1))
+        # Weights:
+        self.weights = []
+        self.weights = [np.random.randn(y, x)  # TODO: rewrite this your own way
+                        for x, y in zip(layer_sizes[:-1], layer_sizes[1:])]
+
+        # Network structure and actual neurons
+        self.layers = [np.zeros(shape=x) for x in layer_sizes]
+        """
+        Example structure for layer_sizes = [5,4,3,2]:
+            o  o    => output layer (with  biases and weights for each neuron to the previous layer)
+           o  o  o    => first hidden layer (with  biases and weights for each neuron to the previous layer)
+          o  o  o  o    => first hidden layer (with  biases and weights for each neuron to the previous layer)
+         o  o  o  o  o    => input layer (no weights or biases assigned, input data is written directly to this layer)
+        """
+
+
